1bbdaf29ef4906c945d82c4fdffd4c9b9829b1d2
Josh DeGrazia <joshdegrazia@gmail.com>
2016-12-04 02:22:57 -0500

    feat: add page links to index page

--- app/javascript/pages/index.jsx
+++ app/javascript/pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import ExternalLinksPanel from 'javascript/components/external_links_panel';
 import CircleImageContainer from 'javascript/components/circle_image_container';
 
@@ -12,7 +13,9 @@ export default React.createClass({
     // the jsx syntax for sublime doesn't play nice with quote characters directly
     // inside divs, so i'm making most text in variables to avoid that
 
-    var introText = `I'm a fourth-year University of Waterloo student studying Computer Science.`;
+    var introText = `I'm a fourth-year University of Waterloo student studying Computer Science.
+                     I seek to develop myself in the field and learn as much about the industry
+                     as I can before I graduate.`;
 
     return (
       <div className='index-root'>
@@ -21,10 +24,21 @@ export default React.createClass({
             <CircleImageContainer src={RedSelfie}/>
             <div className='title-container'>
               <div className='title'>Josh DeGrazia</div>
+              <div className='hcenter subtitle'>Student & Software Developer</div>
               <ExternalLinksPanel/>
             </div>
           </div>
           <div className='intro-description'>{introText}</div>
+          <div>
+            <div>
+              <Link to='/education' className='page-link education'><div className='fa fa-book'/>Education</Link>
+              <Link to='/experience' className='page-link experience'><div className='fa fa-briefcase'/>Work Experience</Link>
+            </div>
+            <div>
+              <Link to='/projects' className='page-link projects'><div className='fa fa-paint-brush'/>Projects</Link>
+              <Link to='/about' className='page-link about'><div className='fa fa-user'/>About Me</Link>
+            </div>
+          </div>
         </div>
       </div>
     );
